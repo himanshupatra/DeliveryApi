f27b20093c3629cdc4a66246324cf1ae61a7742c
Himanshu Patra <50830829+himanshupatra@users.noreply.github.com>
2019-11-11 18:24:21 +0530

    Add files via upload

---
+++ DeliveryApi/src/main/java/com/Lowes/Finance/DeliveryApi/Controller/DeliveryProduct.java
@@ -0,0 +1,16 @@
+package com.Lowes.Finance.DeliveryApi.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+
+
+public class DeliveryProduct {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DeliveryProduct.class);
+
+}
